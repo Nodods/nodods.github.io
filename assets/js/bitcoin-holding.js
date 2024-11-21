9ec705647adca2416a001395c34350ba1dc6a40b
Thandile Nododile <6636345e@gmail.com>
2024-11-21 11:43:04 +0200

    Update bitcoin-holding.js

--- assets/js/bitcoin-holding.js
+++ assets/js/bitcoin-holding.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const btcRateZAR = data.bpi.ZAR.rate_float;
       const btcHoldings = 0.0025; // Example BTC holdings
       const btcValueZAR = btcRateZAR * btcHoldings;
-      const btcChange30d = 0.0016 // Example 7-day change
-      const btcChange1y = 0.0025; // Example 30-day change
+      const btcChange30d = 0.0025 // Example 7-day change
+      const btcChange1y = 0.00391; // Example 30-day change
 
       // Format Bitcoin value with commas
       const formattedBtcValue = btcValueZAR.toLocaleString("en-ZA", {
